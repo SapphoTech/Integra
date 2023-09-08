754cc420a5e6cf27a9703fb6a9e8e999d9f986c8
SapphoTech <144105693+SapphoTech@users.noreply.github.com>
2023-09-08 16:22:42 -0600

    feat: Improve router 0.0.5

--- src/core/router.rs
+++ src/core/router.rs
@@ -1,34 +1,86 @@
 use hyper::{Body, Request, Response, Method};
+use hyper::service::Service;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use std::convert::Infallible;
 use std::future::Future;
 use std::pin::Pin;
-use lazy_static::lazy_static;
+use std::task::Context;
+use tokio::macros::support::Poll;
 
-// The Handler should return a Future
-type AsyncHandler = Arc<dyn Fn(Request<Body>) -> Pin<Box<dyn Future<Output = Response<Body>> + Send + 'static>> + Send + Sync>;
+pub struct ServiceWithRouter {
+    pub router: Arc<Router>,
+}
+
+impl Service<Request<Body>> for ServiceWithRouter {
+    type Response = Response<Body>;
+    type Error = Infallible;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        let router = self.router.clone(); 
+        Box::pin(async move {
+            router.route(req).await
+        })
+    }
+}
+
+pub trait Handler: Send + Sync {
+    fn call(&self, req: Request<Body>) -> Pin<Box<dyn Future<Output = Response<Body>> + Send>>;
+}
+
+impl<F, Fut> Handler for F
+where
+    F: Fn(Request<Body>) -> Fut + Send + Sync,
+    Fut: Future<Output = Response<Body>> + Send + 'static,
+{
+    fn call(&self, req: Request<Body>) -> Pin<Box<dyn Future<Output = Response<Body>> + Send>> {
+        Box::pin(self(req))
+    }
+}
 
 pub struct Route {
     method: Method,
     path: String,
-    handler: AsyncHandler,
+    handler: Arc<dyn Handler>,
 }
 
 impl Route {
-    pub fn get(path: &str, handler_fn: impl Fn(Request<Body>) -> Pin<Box<dyn Future<Output = Response<Body>> + Send + 'static>> + Send + Sync + 'static) {
-        let handler = Arc::new(move |req| handler_fn(req));
-        let route = Route {
+    pub fn get<H: Handler + 'static>(path: &str, handler: H) -> Self {
+        let handler = Arc::new(handler);
+        Route {
             method: Method::GET,
             path: path.to_string(),
-            handler: handler,
-        };
-        ROUTER.register(route);
+            handler,
+        }
+    }
+}
+
+impl Clone for Route {
+    fn clone(&self) -> Self {
+        Route {
+            method: self.method.clone(),
+            path: self.path.clone(),
+            handler: self.handler.clone(),
+        }
+    }
+}
+
+impl Clone for Router {
+    fn clone(&self) -> Self {
+        let routes = self.routes.lock().unwrap();
+        Router {
+            routes: Mutex::new(routes.clone()),
+        }
     }
 }
 
 pub struct Router {
-    routes: Mutex<HashMap<(Method, String), AsyncHandler>>,
+    routes: Mutex<HashMap<(Method, String), Arc<dyn Handler>>>,
 }
 
 impl Router {
@@ -38,24 +90,43 @@ impl Router {
         }
     }
 
-    pub fn register(&self, route: Route) {
+    pub fn register(&mut self, route: Route) {
         let mut routes = self.routes.lock().unwrap();
         routes.insert((route.method, route.path), route.handler);
     }
 
-    pub async fn route(self: Arc<Self>, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    pub async fn route(&self, req: Request<Body>) -> Result<Response<Body>, Infallible> {
         let handler_option = {
             let routes = self.routes.lock().unwrap();
             routes.get(&(req.method().clone(), req.uri().path().to_string())).cloned()
         };
     
         match handler_option {
-            Some(handler) => Ok(handler(req).await),
+            Some(handler) => Ok(handler.call(req).await),
             None => Ok(Response::new(Body::from("Not Found")))
         }
     }
 }
 
-lazy_static! {
-    pub static ref ROUTER: Arc<Router> = Arc::new(Router::new());
+// The routes! macro
+#[macro_export]
+macro_rules! routes {
+    ($($route_slice:expr),*) => {{
+        let mut router = Router::new();
+        $(
+            for route in $route_slice {
+                router.register(route.clone());
+            }
+        )*
+        router
+    }};
+}
+
+#[macro_export]
+macro_rules! route_collector {
+    ($($module_name:ident),*) => {
+        routes!(
+            $(&$module_name::routes()[..]),*
+        )
+    };
 }
